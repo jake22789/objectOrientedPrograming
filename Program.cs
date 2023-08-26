0e7616ae30fb98d8abcd14c4cddb62845a282507
jake22789 <70212338+jake22789@users.noreply.github.com>
2023-08-26 16:03:52 -0600

    SelectionSortDone

--- Program.cs
+++ Program.cs
@@ -1,29 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-void SelectionSort(int[] values){
+using System.Globalization;
+
+void SelectionSort(int[] values)
+{
+    for (int i = 0; i<5;i++)
+    {
+        Console.WriteLine(values[i]);
+    }
+    Console.WriteLine("sort");
     int hold = 0;
     int indi = 0;
-    
-    for (int i = 0; i<(values.Length-1); i++){
-        if (values[i]>hold){
-            hold = values[i];
-            indi = i;
-            Console.Write("new high");
+    for(int J=1;J<values.Length;J++)
+    {
+        for (int i = 0; i<(values.Length-J); i++)
+        {
+            if (values[i]>hold)
+            {
+                hold = values[i];
+                indi = i;
+            }
+        }
+        if(hold > values[values.Length-J])
+        {
+            values[indi] = values[values.Length-J];
+            values[values.Length-J] = hold;
+            hold=0;
         }
     }
-    values[indi] = values[values.Length-1];
-    values[values.Length-1] = hold;
-    
-    for (int i = 0; i<5;i++){
+    for (int i = 0; i<5;i++)
+    {
     Console.WriteLine(values[i]);
-}
+    }
 }
 
 
-int[] n = {5 , 4, 6, 2, 1};
-for (int i = 0; i<5;i++){
-    Console.WriteLine(n[i]);
-}
-Console.WriteLine("sort");
+int[] n = {8 , 10, 3, 5, 1};
 SelectionSort(n);
-
-    
